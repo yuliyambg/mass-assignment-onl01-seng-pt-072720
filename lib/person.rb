919818e575b0bdadc97aad47b8191ec3dfaa60a8
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 01:34:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,11 @@
 class Person
   #your code here
   
-  def initialize
+  attr_accessor
+  
+  def initialize(attributes)
+	    attributes.each {|key, value| self.send(("#{key}="), value)}
+
   end
 
 end
